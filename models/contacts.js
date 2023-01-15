c6dfc8c2101bc5ce6862a00993e826f1337df042
VladOlevskiy <99341001+VladOlevskiy@users.noreply.github.com>
2023-01-15 21:03:32 +0200

    update

--- models/contacts.js
+++ models/contacts.js
@@ -1,63 +1,27 @@
-const fs = require("fs/promises");
-const { nanoid } = require("nanoid");
-const path = require("path");
-// const { nanoid } = require("nanoid");
-// const { constants } = require("buffer");
-const dbPath = path.resolve(__dirname, "contacts.json");
+const mongoose = require("mongoose");
 
-async function readContacts() {
-  const dbRaw = await fs.readFile(dbPath);
-  const dbContacts = JSON.parse(dbRaw);
-  return dbContacts;
-}
+const schema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { versionKey: false }
+);
 
-async function writeDbContacts(db) {
-  await fs.writeFile(dbPath, JSON.stringify(db, null, 2));
-}
-
-const listContacts = async () => {
-  const contactsDb = await readContacts();
-  console.log(contactsDb);
-  return contactsDb;
-};
-
-const getContactById = async (contactId) => {
-  const contactsDb = await readContacts();
-  const contactById = contactsDb.find((contact) => contact.id === contactId);
-  return contactById || null;
-};
-
-const removeContact = async (contactId) => {
-  const contactsDb = await readContacts();
-  const updateDb = contactsDb.filter((contact) => contact.id !== contactId);
-  await writeDbContacts(updateDb);
-};
-
-const addContact = async ({ name, email, phone }) => {
-  const id = nanoid();
-  const newContact = { id, name, email, phone };
-  const contactsDb = await readContacts();
-  contactsDb.push(newContact);
-  await writeDbContacts(contactsDb);
-  return newContact;
-};
-
-const updateContact = async (contactId, name, email, phone) => {
-  const contactsDb = await readContacts();
-  contactsDb.forEach((contact) => {
-    if (contact.id === contactId) {
-      contact.name = name;
-      contact.email = email;
-      contact.phone = phone;
-    }
-  });
-  await writeDbContacts(contactsDb);
-};
+const Contact = mongoose.model("contacts", schema);
 
 module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
+  Contact,
 };
